8b317bc9340af93429380764f6a30f03215e5664
Stanislav <stasikbygaev@gmail.com>
2025-09-02 22:37:17 +0500

    Enhance Stripe payment validation: restrict subscriptions to monthly plan only for Stripe payment method

--- src/main/java/com/winworld/coursestools/validation/validator/payment/impl/StripePaymentValidator.java
+++ src/main/java/com/winworld/coursestools/validation/validator/payment/impl/StripePaymentValidator.java
@@ -4,6 +4,7 @@ import com.winworld.coursestools.entity.Order;
 import com.winworld.coursestools.entity.user.User;
 import com.winworld.coursestools.entity.user.UserSubscription;
 import com.winworld.coursestools.enums.PaymentMethod;
+import com.winworld.coursestools.enums.Plan;
 import com.winworld.coursestools.exception.exceptions.ConflictException;
 import com.winworld.coursestools.validation.validator.payment.PaymentValidator;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +25,9 @@ public class StripePaymentValidator implements PaymentValidator {
     public void validate(User user, Order order, UserSubscription userSubscription) {
         if (userSubscription == null || userSubscription.getStatus().equals(GRACE_PERIOD) || userSubscription.getIsTrial()) {
             return;
+        }
+        if (!order.getPlan().getName().equals(Plan.MONTH)) {
+            throw new ConflictException("With Stripe payment method, you can only subscribe to the monthly plan.");
         }
         if (userSubscription.getPaymentMethod().equals(order.getPaymentMethod()) && order.getPaymentMethod().equals(STRIPE)) {
             throw new ConflictException("You already have an active subscription with Stripe payment method.");
